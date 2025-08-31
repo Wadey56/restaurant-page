95f0c08b5c369236bd141a8128df2ace90368da9
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2025-08-31 10:38:48 +0100

    Load in text content for serving section via DOM

--- src/home.js
+++ src/home.js
@@ -27,15 +27,26 @@ foodSpread.alt = "Food Spread";
 let servingSection = document.createElement("div");
 servingSection.classList.add("serving-section");
 
+let servingSectionHeaders = ["Boss Battle Breakfast", "Level-up Lunch", "Daring Dinner Duels"];
+let servingSectionStats = ["+12 Saving Throws", "+5 Stanima", "+10 Dexterity"];
+let servingSectionDescriptions = ["Are you in need of hit points for your next fight? Of course you are! Here at our diner we have everything you would need to slay not only your hunger but that pesky boss you just cant seem to defeat recently...", "Here at our diner we know real energy is created from eating delicious lunches and not from collecting floating orbs of XP; so come visit Delicious in Diner to rest up and level-up for that weary journey ahead.", "Devour to your heart's desire; whether you want to play it safe and try our low level relished meals such as the Golum Field Fresh Veggie Lunch or dare your guts with our high level spiced platters like our Dwarf-Style Stir Fry-whatever your seasoning skillset, we won't judge!"];
+
 for (let i = 0; i < 3; i++) {
     let servingWrapper = document.createElement("div");
     servingWrapper.classList.add("serving-wrapper");
+
     let servingHeader = document.createElement("h2");
     servingHeader.classList.add("serving-header");
+    servingHeader.textContent = servingSectionHeaders[i];
+
     let servingStats = document.createElement("em");
     servingStats.classList.add("serving-stats");
+    servingStats.textContent = servingSectionStats[i];
+
     let servingDescription = document.createElement("p");
     servingDescription.classList.add("serving-description");
+    servingDescription.textContent = servingSectionDescriptions[i];
+
     servingWrapper.appendChild(servingHeader);
     servingWrapper.appendChild(servingStats);
     servingWrapper.appendChild(servingDescription);
