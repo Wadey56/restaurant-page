bef61d2c3452d80f08cae629cbf2929cffce81d2
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2025-09-13 22:02:26 +0100

    Add event listeners for buttons to build page content

--- src/index.js
+++ src/index.js
@@ -5,4 +5,21 @@ import { loadHomeContent } from "./home";
 import { loadMenuContent } from "./menu";
 
 loadHomeContent();
-loadMenuContent();
+
+let homeButton = document.getElementById("home-button");
+homeButton.addEventListener("click", () => {
+    content.innerHTML = "";
+    loadHomeContent();
+});
+
+let menuButton = document.getElementById("menu-button");
+menuButton.addEventListener("click", () => {
+    content.innerHTML = "";
+    loadMenuContent();
+});
+
+// let aboutButton = document.getElementById("about-button");
+// aboutButton.addEventListener("click", () => {
+//     content.innerHTML = "";
+//     loadAboutContent();
+// });
