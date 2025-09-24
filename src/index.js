553244a0ee67aa4c9b74cca7d15049e88ce7a383
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2025-09-24 21:59:22 +0100

    Comment out DOM to build About on html

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import "./globalStyles.css";
 import "./homeStyles.css";
 import "./menuStyles.css";
+import "./aboutStyles.css";
 import { loadHomeContent } from "./home";
 import { loadMenuContent } from "./menu";
 
-loadHomeContent();
+// loadHomeContent();
+// loadMenuContent();
 
-let homeButton = document.getElementById("home-button");
-homeButton.addEventListener("click", () => {
-    content.innerHTML = "";
-    loadHomeContent();
-});
+// let homeButton = document.getElementById("home-button");
+// homeButton.addEventListener("click", () => {
+//     content.innerHTML = "";
+//     loadHomeContent();
+// });
 
-let menuButton = document.getElementById("menu-button");
-menuButton.addEventListener("click", () => {
-    content.innerHTML = "";
-    loadMenuContent();
-});
+// let menuButton = document.getElementById("menu-button");
+// menuButton.addEventListener("click", () => {
+//     content.innerHTML = "";
+//     loadMenuContent();
+// });
 
 // let aboutButton = document.getElementById("about-button");
 // aboutButton.addEventListener("click", () => {
