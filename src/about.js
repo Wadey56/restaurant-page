ca4c2367af8edad04ddd1daed97f29f057ff9ebc
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2025-09-26 21:43:24 +0100

    Fix about wrapper id and add description text

--- src/about.js
+++ src/about.js
@@ -5,7 +5,7 @@ aboutHeader.id = "about-header";
 aboutHeader.textContent = "About";
 
 let aboutWrapper = document.createElement("div");
-aboutHeader.id = "about-wrapper";
+aboutWrapper.id = "about-wrapper";
 
 let senshiImage = document.createElement("img");
 senshiImage.id = "senshi-image";
@@ -17,7 +17,13 @@ aboutWrapper.appendChild(senshiImage);
 let aboutContent = document.createElement("div");
 aboutContent.id = "about-content";
 
+aboutWrapper.appendChild(aboutContent);
 
+let aboutDescription = document.createElement("p");
+aboutDescription.classList = "about-description";
+aboutDescription.textContent = "Delicious in Diner was founded by our very own Chef Senshi. He has been serving up delicious meals to adventurers since 2014. His passion for both cooking and the dungeon has inspired the great menu of alternative ingredient meals and BYOI services. All our ingredients are sourced from local adventurers and the menu is pre-approved by his trusty party of mixed characters to appeal to the wider community."
+
+aboutContent.appendChild(aboutDescription);
 
 let content = document.getElementById("content");
 
